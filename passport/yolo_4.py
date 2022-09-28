4a52e83483be4eac81127fe7e2d558de11d42411
nosovdmitry007 <nosov.dmitry@yahoo.com>
2022-09-28 17:13:03 +0300

    Add_auto_rotation

--- passport/yolo_4.py
+++ passport/yolo_4.py
@@ -30,7 +30,7 @@ def yolo_4(img):
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.3:
-                # Object detected
+                # Object detected/
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
