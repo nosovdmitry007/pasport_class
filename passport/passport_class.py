78f65eb9fc8c9cdb5c6cd09b80beca37d121c137
nosovdmitry007 <nosov.dmitry@yahoo.com>
2022-10-26 13:32:34 +0300

    ADD yolo 5

--- passport/passport_class.py
+++ passport/passport_class.py
@@ -13,7 +13,14 @@ class Passport:
                         gpu=False)
         self.model_round = torch.hub.load('yolov5_master', 'custom', path='yolo5/rotation.pt', source='local')
         self.model_detect = torch.hub.load('yolov5_master', 'custom', path='yolo5/detect.pt', source='local')
+        self.path_img = "LapSRN/LapSRN_x2.pb"
 
+    def resiz(self,img):
+        sr = cv2.dnn_superres.DnnSuperResImpl.create()
+        sr.readModel(self.path_img)
+        sr.setModel("lapsrn",2)
+
+        return sr.upsample(img)
     def zero(self,n):
         return n * (n > 0)
 
@@ -137,9 +144,16 @@ class Passport:
         ver = 0
         for i, v in oblasty.items():
             image = cv2.cvtColor(v, cv2.COLOR_BGR2RGB)
+            if image.shape[0] <=25:
+                image = self.threshold_image(image)
+            cv2.imwrite('test/'+i+'.jpg',image)
             # Для каждого класса устанавливаем свои ограничения на распознания классов
-            if 'date' in i or 'code' in i or 'series' in i:
-                result = self.reader.readtext(image, allowlist='0123456789-. ')
+            if 'date' in i:
+                result = self.reader.readtext(image, allowlist='0123456789.')
+            if 'code' in i:
+                result = self.reader.readtext(image, allowlist='0123456789-')
+            if 'series' in i:
+                result = self.reader.readtext(image, allowlist='0123456789 ')
             elif 'surname' in i or 'name' in i or 'patronymic' in i:
                 result = self.reader.readtext(image,
                                          allowlist='АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-')
@@ -150,7 +164,7 @@ class Passport:
                                         allowlist='АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-"№ .1234567890')
             pole = ''
             for k in range(len(result)):
-                if result[k][2] * 100 >= 35:
+                if result[k][2] * 100 >= 25:
                     if str(result[k][1]).isnumeric():
                         if result[k][2] * 100 >= 70:
                             pole = pole + ' ' + str(result[k][1])
@@ -268,50 +282,57 @@ class Passport:
 class INN(Passport):
     def __init__(self):
         self.reader = easyocr.Reader(['ru'],
-                        model_storage_directory='EasyOCR/model',
-                        user_network_directory='EasyOCR/user_network',
-                        recog_network='custom_example',
-                        gpu=False)
+                                     model_storage_directory='EasyOCR/model',
+                                     user_network_directory='EasyOCR/user_network',
+                                     recog_network='custom_example',
+                                     gpu=False)
+
         self.model_round_inn = torch.hub.load('yolov5_master', 'custom', path='yolo5/inn_rotation.pt', source='local')
         self.model_detect_inn = torch.hub.load('yolov5_master', 'custom', path='yolo5/fio_INN.pt', source='local')
-    def result_inn(self,img):
+        self.model_numbers = torch.hub.load('yolov5_master', 'custom', path='yolo5/best_X_INN.pt', source='local')
+
+    def result_inn(self, img):
         return self.model_round_inn(img)
 
-    def get_angle_rotation_inn(self,centre, point, target_angle):
-        new_point =(point[0] - centre[0], point[1] - centre[1])  #передвигаем центр системы координат в точку центр. у нее будет (0,0) ищем новые координаты у точки point
-        a,b = new_point[0], new_point[1]
-        res = math.atan2(b,a) #ищем полярный угол у new_point
-        if (res < 0) :
-              res += 2 * math.pi
-        return (math.degrees(res)+target_angle) % 360  #возвращаем угол поворота для cv2
+    def get_angle_rotation_inn(self, centre, point, target_angle):
+        new_point = (point[0] - centre[0], point[1] - centre[
+            1])  # передвигаем центр системы координат в точку центр. у нее будет (0,0) ищем новые координаты у точки point
+        a, b = new_point[0], new_point[1]
+        res = math.atan2(b, a)  # ищем полярный угол у new_point
+        if (res < 0):
+            res += 2 * math.pi
+        return (math.degrees(res) + target_angle) % 360  # возвращаем угол поворота для cv2
 
     def get_image_after_rotation_inn(self, img, point1, point2):
         results = self.result_inn(img)
         pd = results.pandas().xyxy[0]
-        pd = pd.assign(centre_x = pd.xmin + (pd.xmax-pd.xmin)/2)
-        pd = pd.assign(centre_y = pd.ymin + (pd.ymax-pd.ymin)/2)
-        tmp = pd.loc[pd['name']==point1]
+        pd = pd.assign(centre_x=pd.xmin + (pd.xmax - pd.xmin) / 2)
+        pd = pd.assign(centre_y=pd.ymin + (pd.ymax - pd.ymin) / 2)
+        tmp = pd.loc[pd['name'] == point1]
         LEFT, RIGHT = None, None
         for index, row in tmp.iterrows():
             LEFT = (row['centre_x'], row['centre_y'])
             break
-        #получим координаты верха, там где печать
-        tmp = pd.loc[pd['name']==point2]
+        # получим координаты верха, там где печать
+        tmp = pd.loc[pd['name'] == point2]
         for index, row in tmp.iterrows():
             RIGHT = (row['centre_x'], row['centre_y'])
             break
-        if LEFT == None or RIGHT == None: #похоже там нет нужных нам строк
+        if LEFT == None or RIGHT == None:  # похоже там нет нужных нам строк
             return img
 
         angle = self.get_angle_rotation_inn(LEFT, RIGHT, 0)
-        img = self.rotate_image(img, angle)  #вращаем той процедурой, что выше
+        img = self.rotate_image(img, angle)  # вращаем той процедурой, что выше
         return img
-    def get_crop_inn(self,file):
+
+    def get_crop_inn(self, file):
         image = cv2.imread(file)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #сразу в серый
-        image = self.get_image_after_rotation_inn(image, point1 = 'sv', point2 = 'vo') #передаем в процедуру метки, котоыре будут использоваться для вращения первая - левая метка, вторая правая, будут ставиться в горизонт
-        image = self.get_image_after_rotation_inn(image, point1 = 'lev', point2 = 'prav')
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # сразу в серый
+        image = self.get_image_after_rotation_inn(image, point1='sv',
+                                                  point2='vo')  # передаем в процедуру метки, котоыре будут использоваться для вращения первая - левая метка, вторая правая, будут ставиться в горизонт
+        image = self.get_image_after_rotation_inn(image, point1='lev', point2='prav')
         return image
+
     def yolo_5_inn(self, img):
         results = self.model_detect_inn(img)
         df = results.pandas().xyxy[0]
@@ -320,8 +341,8 @@ class INN(Passport):
         ob['class'] = df['name']
         ob['x'] = df['xmin']
         ob['y'] = df['ymin']
-        ob['w'] = df['xmax']-df['xmin']
-        ob['h'] = df['ymax']-df['ymin']
+        ob['w'] = df['xmax'] - df['xmin']
+        ob['h'] = df['ymax'] - df['ymin']
         oblasty = ob.values.tolist()
         return img, oblasty
 
@@ -336,8 +357,58 @@ class INN(Passport):
             w = int(l[3])
             ob = cat
             oblasty[ob] = image[self.zero(y - math.ceil(h * 0.1)):y + math.ceil(h * 1.1),
-                      self.zero(x - math.ceil(w * 0.03)):x + math.ceil(w * 1.03)]
+                          self.zero(x - math.ceil(w * 0.03)):x + math.ceil(w * 1.03)]
         return oblasty
+
+    def Intersection(self, boxA, boxB):
+        # determine the (x, y)-coordinates of the intersection rectangle
+        xA = max(boxA[0], boxB[0])
+        yA = max(boxA[1], boxB[1])
+        xB = min(boxA[2], boxB[2])
+        yB = min(boxA[3], boxB[3])
+
+        # compute the area of intersection rectangle
+        interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+        return interArea
+
+    def IoU(self, boxA, boxB):
+        # compute the area of intersection rectangle
+        interArea = self.Intersection(boxA, boxB)
+
+        # compute the area of both the prediction and ground-truth rectangles
+        boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+        boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+        # calculation iou
+        iou = interArea / float(boxAArea + boxBArea - interArea)
+
+        # return the intersection over union value
+        return iou
+
+    def numbers_detect(self, img):
+
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        result = self.model_numbers(image)
+
+        final_res = result.pandas().xyxy[0]
+        result = result.pandas().xyxy[0]
+
+        # delete intersection objects
+        for obj1 in range(len(result)):
+            for obj2 in range(len(result)):
+
+                boxA = result.iloc[obj1, :4].values
+                boxB = result.iloc[obj2, :4].values
+
+                if boxA.all() != boxB.all():
+                    if self.IoU(boxA, boxB) > 0.2:
+                        if result.iloc[obj1, 4] > result.iloc[obj2, 4]:
+                            final_res = final_res[final_res.xmin != result.iloc[obj2, 0]]
+
+                        else:
+                            final_res = final_res[final_res.xmin != result.iloc[obj1, 0]]
+
+        return final_res
 
     def recognition_slovar_inn(self, oblasty):
         data = {}
@@ -345,28 +416,43 @@ class INN(Passport):
         d = {}
         for i, v in oblasty.items():
             image = cv2.cvtColor(v, cv2.COLOR_BGR2RGB)
+
+            # plt.imshow(image)
+            # plt.show()
+
             if 'inn' in i:
-                result = self.reader.readtext(image, allowlist='0123456789')
+
+                numbs = self.numbers_detect(image).sort_values(by=['xmin']).iloc[:, 5].values
+                res_numbs = ''.join(str(e) for e in numbs)
+
+                result = [([], f'{res_numbs}', 0.0)]
+
             elif 'fio' in i:
                 result = self.reader.readtext(image,
-                                         allowlist='АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ- ')
+                                              allowlist='АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ- ')
+
             pole = ''
             for k in range(len(result)):
                 pole = pole + ' ' + str(result[k][1])
             if pole:
                 pole = pole.strip()
-                if 'inn' in i:
-                    pole = pole.replace(' ','')
                 d[i.split('.', 1)[0]] = pole.upper().strip()
         data['inn'].append(d)
         return data['inn']
 
-    def detect_inn(self,photo):
-        pole = ['fio','inn']
-
+    def detect_inn(self, photo):
+        pole = ['fio', 'inn']
         croped = self.get_crop_inn(photo)
+
         if croped != '':
             img, detect = self.yolo_5_inn(croped)
+
+            for l in detect:
+                x = l[1]
+                y = l[2]
+                w = l[3]
+                h = l[4]
+
             obl = self.oblasty_yolo_5_inn(img, detect)
             rec = self.recognition_slovar_inn(obl)
             key = list(rec[0].keys())
@@ -378,11 +464,7 @@ class INN(Passport):
                     flag = 0
             else:
                 flag = 1
-            if flag == 1:
-                rec = {}
-            else:
-                rec = rec[0]
-            return rec, flag
+            return rec[0], flag
         else:
             rec = {}
             return rec, 1
